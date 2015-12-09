13e1fcd2c5b5afd9c4ac4351f0ce2e6ced97a2d8
Johannes Ewald <johannes.ewald@peerigon.com>
2015-12-09 15:11:54 +0100

    Add test print script

---
+++ test/print.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var unicons = require("../lib/index.js");
+
+Object.keys(unicons)
+    .filter(function (key) {
+        return typeof unicons[key] === "string";
+    })
+    .forEach(function (icon) {
+        console.log(icon, unicons.cli(icon));
+    });
