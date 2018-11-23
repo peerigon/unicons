ed964fb725fcbe2625a3ebef76ba1055370f0d2e
qcgm1978 <zhanghl@strongunion.com.cn>
2018-11-23 10:35:22 +0800

    ZDings

--- test/print.js
+++ test/print.js
@@ -8,13 +8,16 @@ Object.keys(unicons)
     })
     .forEach(function (icon) {
         const iconFromCode = unicons.cli(icon)
-        console.log(icon, iconFromCode);
+        // console.log(icon, iconFromCode);
         if (icon === 'apple') {
             it(`apple icon`, () => {
                 expect(iconFromCode).toBe(unicons.apple).toBe('')
             });
         }
     });
+it(`stop`, () => {
+    expect(unicons.cli('ZDings')).toBe('✏')
+})
 it(`img`, () => {
     expect(unicons.img(100)).toBe('https://abs.twimg.com/emoji/v2/72x72/1f4af.png')
     expect(unicons.img('stop')).toBe('https://abs.twimg.com/emoji/v2/72x72/1f64c.png')
