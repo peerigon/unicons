8232e9d180ac26802b0d63d0564d33e722881c54
qcgm1978 <zhanghl@strongunion.com.cn>
2018-11-27 10:21:03 +0800

    ditto

--- test/print.js
+++ test/print.js
@@ -19,8 +19,11 @@ it(`The possible code point values range from U+0000 to U+10FFFF.`, () => {
     expect(unicons.cli('ZDings')).toBe('✏')
     expect(unicons.cli('dog')).toBe('🐶')
     expect(unicons.cli('laughing')).toBe('🤣')
-    expect(unicons.cli('rocket')).toBe('🚀')
 })
+it(`In ECMAScript 6 this will be easy, since it introduces a new type of escape sequence: Unicode code point escapes.`, () => {
+    expect(unicons.cli('rocket')).toBe("\uD83D\uDE80").toBe('🚀')
+
+});
 it(`img`, () => {
     expect(unicons.img(100)).toBe('https://abs.twimg.com/emoji/v2/72x72/1f4af.png')
     expect(unicons.img('stop')).toBe('https://abs.twimg.com/emoji/v2/72x72/1f64c.png')
